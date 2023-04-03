245ca525fe7c2025bdeb354d174d55d6a1f38ca7
Karolis <karolisburtazodis@gmail.com>
2023-04-03 15:53:46 +0300

    feat: add chart for data for average age of people with gaming pc

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <script setup>
   import { ref } from "vue";
   import Home from "./components/Home.vue"
@@ -5,21 +6,22 @@
 
   let activeTab = ref('Home');
 </script>
-
 <template>
   <div id="app-body">
     <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand ps-4" href="#">Netflix Data</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a @click="activeTab = 'Home'" class="nav-item nav-link active" href="#">Home</a>
-            <a @click="activeTab = 'Chart'" class="nav-item nav-link" href="#">Chart</a>
-            <a @click="activeTab = 'About'" class="nav-item nav-link" href="#">About</a>
-          </div>
+      <a class="navbar-brand ps-4" href="#">
+        <img src="src\assets\chart2.png" alt="Netflix App" />
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a @click="activeTab = 'Home'" class="nav-item nav-link active" href="#"><i class="bi bi-house-door-fill me-2"></i>Home</a>
+          <a @click="activeTab = 'Chart'" class="nav-item nav-link" href="#"><i class="bi bi-bar-chart-line-fill me-2"></i>Chart</a>
+          <a @click="activeTab = 'About'" class="nav-item nav-link disabled" href="#"><i class="bi bi-info-circle-fill me-2"></i>About</a>
         </div>
+      </div>
     </nav>
 
     <home v-if="activeTab === 'Home'"/>
@@ -31,19 +33,21 @@
     </div>
 
     <footer class="footer bg-dark text-white py-2">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 d-flex align-items-center gap-2">
-              <a href="https://github.com/Saigunas/Chart-website"><h3 class="m-0 bi bi-github"></h3></a>
-              <span>University project for data visualization</span>
-            </div>
-            <div class="col-md">
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 d-flex align-items-center gap-2">
+            <a href="https://github.com/Saigunas/Chart-website"><i class="bi bi-github h3 m-0"></i></a>
+            <span>University project for data visualization</span>
+            <p class="footer-information">Project was made by Karolis, Gvidas, Saigunas.</p>
+          </div>
+          <div class="col-md">
           </div>
         </div>
+      </div>
     </footer>
   </div>
 </template>
+
 
 <style scoped>
 #app-body {
@@ -51,5 +55,37 @@
   flex-direction: column;
   justify-content: space-between;
   min-height: 100vh;
+}
+.footer-information{
+  position:relative;
+  left: 750px;
+}
+
+.navbar-nav a:hover {
+  transform: translateY(-5px);
+}
+
+.nav-item{
+  transition: transform 0.3s ease, border-bottom 0.3s ease;
+}
+
+.nav-item:hover{
+  transform: translateY(-5px);
+}
+
+
+.navbar-nav a:hover::after {
+  content: '';
+  display: block;
+  height: 2px;
+  width: 100%;
+  background-color: orangered;
+  position: absolute;
+  bottom: -5px;
+  left: 0;
+}
+
+.navbar {
+  animation: slideInDown 0.5s ease;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,123 @@
-<template>
-    Home
-</template>
-
 <script setup>
   import { ref } from "vue";
+  import Chart from "./Chart.vue"
+
+  let activeTab = ref('Home');
+
 </script>
 
+<template>
+  <div class="about-container">
+    <h2>Welcome!</h2>
+    <div class="about-border"></div>
+    <p>
+      This is a university project for data visualization. We thought that it would be a great idea, instead of using
+      a program such as Excel that works with data utilization and representation,
+      Instead we would use programming language JavaScript and Vue.JS Framework
+    </p>
+    <h2>Our Charts</h2>
+    
+    <div class="about-border"></div>
+      <p>
+        Head over the <a @click="activeTab = 'Chart'" class="chart-view" href="#">Chart</a>  tab to check our Charts made about Netflix viewership, where we've visualized almost a decade worth of Information
+        About User's activity, country population, most popular genres and many more!
+        <chart v-if="activeTab === 'Chart'"/>
+    </p>
+  </div>
+  
+  </template>
+
 <style>
+.about-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 20px;
+}
+
+.chart-view {
+  text-decoration: underline;
+  color: blue;
+}
+
+
+.about-border {
+  height: 5px;
+  width: 60%;
+  background-color: black;
+  margin-bottom: 30px;
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 30px;
+  margin-top: 0;
+  transition: margin-top 0.5s ease, transform 0.3s ease, border-bottom 0.3s ease;
+}
+
+h2:hover {
+  margin-top: -15px;
+  transition: margin-top 0.5s ease, transform 0.3s ease, border-bottom 0.3s ease;
+
+}
+
+p {
+  padding: 20px;
+  width: 80%;
+  text-align: center;
+  line-height: 1.5;
+  color: gray
+}
+
+.content,
+h2 {
+  animation: fadeIn 0.5s ease;
+}
+
+.section {
+  position: relative;
+  margin-bottom: 50px;
+  animation: slideInUp 0.5s ease;
+}
+
+.section .title {
+  position: absolute;
+  top: -50px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #fff;
+  padding: 0 20px;
+  font-size: 20px;
+  font-weight: bold;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  animation: slideInDown 0.5s ease;
+}
+
+@keyframes slideInDown {
+  0% {
+    transform: translateY(-100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slideInUp {
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
